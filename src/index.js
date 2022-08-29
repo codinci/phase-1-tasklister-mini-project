8a8a6de3a6d1224f06ef3e04522a370ed8cc3da1
codinci <dkibau@gmail.com>
2022-08-29 11:03:45 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('#create-task-form');
+  form.addEventListener('submit',(e) =>{
+    e.preventDefault();
+    buildToDo(e.target.new_task_description.value);
+    form.reset()
+  })
+})
+
+function buildToDo(todo) {
+  let todoList = document.querySelector('#tasks');
+  let todoActivity = document.createElement('li');
+  let deleteBtn = document.createElement('button');
+  deleteBtn.addEventListener('click', handleDelete);
+  todoActivity.textContent = `${todo} `;
+  deleteBtn.textContent = ' x ';
+  todoActivity.appendChild(deleteBtn);
+  todoList.appendChild(todoActivity);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
